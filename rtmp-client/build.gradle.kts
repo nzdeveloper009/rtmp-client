bfcf1e3444cb9a79fc69fdf07b8927edb64581bb
Syed Nokhaiz Al Hassan <nzdeveloper009@gmail.com>
2024-10-23 17:32:25 +0500

    update - v

--- rtmp-client/build.gradle.kts
+++ rtmp-client/build.gradle.kts
@@ -56,7 +56,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.github.nzdeveloper009"
                 artifactId = "rtmp-client"
-                version = "1.0.0"
+                version = "1.0.1"
             }
         }
     }
